afa57ec8a58d19aed05d896db08af129e9999d55
Wi-Go <akaza.xeros@gmail.com>
2023-11-28 11:15:58 +0100

    Fetching games through a custom hook implemented

--- src/components/UI/GameGrid.tsx
+++ src/components/UI/GameGrid.tsx
@@ -1,39 +1,8 @@
 import { Spinner, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import apiClient from '../../sources/apiClient.ts';
-
-type Game = {
-  id: number;
-  name: 'string';
-  slug: 'string';
-  image: 'string';
-  image_background: 'string';
-  games_count: number;
-};
-
-type FetchedGamesRes = {
-  count: number;
-  results: Game[];
-};
+import useGames from '../../hooks/useGames';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get<FetchedGamesRes>('/games')
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
+  const { games, error, isLoading } = useGames();
 
   if (isLoading) return <Spinner />;
 
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../sources/apiClient';
+import { CanceledError } from 'axios';
+
+export type Game = {
+  id: number;
+  name: 'string';
+  slug: 'string';
+  image: 'string';
+  image_background: 'string';
+  games_count: number;
+};
+
+export type FetchedGamesRes = {
+  count: number;
+  results: Game[];
+};
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchedGamesRes>('/games', { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, setGames, error, setError, isLoading };
+};
+
+export default useGames;
