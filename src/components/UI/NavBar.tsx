4654cca9a8cc764aa9c7803baa7cecf0c0563769
AkazaXeros <akaza.xeros@gmail.com>
2024-12-16 20:28:46 -0500

    Change logo

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,6 +1,6 @@
 import { HStack, Image } from '@chakra-ui/react';
 
-import logo from '../../assets/logo.jpg';
+import logo from '../../assets/logo.png';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 import { Link } from 'react-router-dom';
@@ -12,6 +12,7 @@ const NavBar = () => {
         <Image
           src={logo}
           boxSize="2.5rem"
+          width={{ base: 'auto', md: '3rem' }}
           alt="logo"
           borderRadius={10}
           objectFit={'cover'}
