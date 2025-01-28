6823026050a58eb705b9769cc07240958ebf1c2a
William G <127890894+AkazaXeros@users.noreply.github.com>
2025-01-28 14:14:20 -0500

    Update SearchInput.tsx

--- src/components/UI/SearchInput.tsx
+++ src/components/UI/SearchInput.tsx
@@ -1,35 +1,36 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
-import { useRef } from 'react';
-import { BsSearch } from 'react-icons/bs';
-import useGameQueryStore from '../../store';
-import { useNavigate } from 'react-router-dom';
+import { Box, HStack, Image } from '@chakra-ui/react';
 
-const SearchInput = () => {
-  const ref = useRef<HTMLInputElement>(null);
-  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+import logo from '../../assets/logo.png';
+import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
-  const navigate = useNavigate();
-
+const NavBar = () => {
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (ref.current) {
-          setSearchText(ref.current.value);
-          navigate('/');
-        }
-      }}
-    >
-      <InputGroup>
-        <InputLeftElement children={<BsSearch />} />
-        <Input
-          ref={ref}
-          borderRadius={20}
-          placeholder="Search games..."
-          variant="filled"
-        />
-      </InputGroup>
-    </form>
+    <HStack padding="0.8rem">
+      <Box flex={{ base: 1, md: 0.5, lg: '10%' }}>
+        <Link to="/">
+          <Image
+            src={logo}
+            boxSize="2.5rem"
+            width={{ base: 'auto', md: '3rem' }}
+            alt="logo"
+            borderRadius={10}
+            objectFit={'cover'}
+          />
+        </Link>
+      </Box>
+      <Box flex={{ base: 4, lg: '80%' }}>
+        <SearchInput />
+      </Box>
+
+      <Box
+        flex={{ sm: 0.5, md: 1, lg: '10%' }}
+        marginLeft={{ sm: 10, md: 15, lg: 20 }}
+      >
+        <ColorModeSwitch />
+      </Box>
+    </HStack>
   );
 };
-export default SearchInput;
+export default NavBar;
